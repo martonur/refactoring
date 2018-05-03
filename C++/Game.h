d6a7f77bcde89023bfddf86674741004a9a8742e
Márton <mrm4rc1@gmail.com>
2018-05-03 20:49:46 +0200

    Rearrange Game class' members
    - Move all members to one public and one private access modifier
    - Code formatting

--- C++/Game.h
+++ C++/Game.h
@@ -9,42 +9,31 @@ using namespace std;
 
 
 class Game{
-
-		private:
-			vector<string> players;
-
-			int places[6];
-			int purses[6];
-
-			bool inPenaltyBox[6];
-
-			list<string> popQuestions;
-			list<string> scienceQuestions;
-			list<string> sportsQuestions;
-			list<string> rockQuestions;
-
-			int currentPlayer;
-			bool isGettingOutOfPenaltyBox;
-
-public:
-	Game();
-	string createRockQuestion(int index);
-	bool isPlayable();
-	bool add(string playerName);
-
-	int howManyPlayers();
-	void roll(int roll);
+	public:
+		Game();
+		string createRockQuestion(int index);
+		bool isPlayable();
+		bool add(string playerName);
+		int howManyPlayers();
+		void roll(int roll);
+		bool wasCorrectlyAnswered();
+		bool wrongAnswer();
 
 	private:
 		void askQuestion();
 		string currentCategory();
-
-				public:
-					bool wasCorrectlyAnswered();
-					bool wrongAnswer();
-
-private:
-	bool didPlayerWin();
+		bool didPlayerWin();
+		vector<string> players;
+		int places[6];
+		int purses[6];
+		bool inPenaltyBox[6];
+		list<string> popQuestions;
+		list<string> scienceQuestions;
+		list<string> sportsQuestions;
+		list<string> rockQuestions;
+		int currentPlayer;
+		bool isGettingOutOfPenaltyBox;
+		
 };
 
 #endif /* GAME_H_ */
